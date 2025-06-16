b8aeab9faf38ee499b79768e64d2bceae1b6cdee
jmbcode23 <jonathanmilolocode@gmail.com>
2025-06-16 14:00:17 +0200

    Fixed the Shadcn Card component

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -1,6 +1,6 @@
-import * as React from "react";
+import * as React from "react"
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
 function Card({ className, ...props }) {
   return (
@@ -12,7 +12,7 @@ function Card({ className, ...props }) {
       )}
       {...props}
     />
-  );
+  )
 }
 
 function CardHeader({ className, ...props }) {
@@ -25,7 +25,17 @@ function CardHeader({ className, ...props }) {
       )}
       {...props}
     />
-  );
+  )
+}
+
+function CardTitle({ className, ...props }) {
+  return (
+    <div
+      data-slot="card-title"
+      className={cn("leading-none font-semibold", className)}
+      {...props}
+    />
+  )
 }
 
 function CardDescription({ className, ...props }) {
@@ -35,5 +45,48 @@ function CardDescription({ className, ...props }) {
       className={cn("text-muted-foreground text-sm", className)}
       {...props}
     />
-  );
+  )
+}
+
+function CardAction({ className, ...props }) {
+  return (
+    <div
+      data-slot="card-action"
+      className={cn(
+        "col-start-2 row-span-2 row-start-1 self-start justify-self-end",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function CardContent({ className, ...props }) {
+  return (
+    <div
+      data-slot="card-content"
+      className={cn("px-6", className)}
+      {...props}
+    />
+  )
+}
+
+function CardFooter({ className, ...props }) {
+  return (
+    <div
+      data-slot="card-footer"
+      className={cn("flex items-center px-6 [.border-t]:pt-6", className)}
+      {...props}
+    />
+  )
+}
+
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
 }
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import __dirname from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
